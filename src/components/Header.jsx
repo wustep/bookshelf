0af02484fd99e1cd1e7bdec4448cc1d2312296da
Stephen Wu <wujstephen@gmail.com>
2025-12-29 18:11:35 -0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,9 @@ export function Header() {
 					<span className="header__icon">✦</span>
 					<h1 className="header__title">bookshelf</h1>
 				</div>
-				<p className="header__subtitle">a curated collection of design books</p>
+				<p className="header__subtitle">
+					a curated collection of my favorite books
+				</p>
 			</div>
 		</header>
 	)
